1827af59a3fcd238b472badcff23655d72f56520
Adrian Johnson <adrianqjohnson@gmail.com>
2025-02-05 23:57:43 -0500

    Created navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
+import { NavigationComponent } from './shared/navigation/navigation.component';
 import { RouterOutlet } from '@angular/router';
-import { RecipeComponent } from './pages/recipe/recipe.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RecipeComponent],
+  imports: [RouterOutlet, NavigationComponent],
   template: `
+    <app-navigation />
     <main class="main">
-      <div class="content">
-        <router-outlet />
-        <app-recipe></app-recipe>
-      </div>
+      <router-outlet />
     </main>
-  `,
-  styleUrl: './app.component.scss'
+  `
 })
 export class AppComponent {
   title = 'simplyrecipe';
