9ae19a80507d502895a34c81fc5492353b70d8ec
Adrian Johnson <adrianqjohnson@gmail.com>
2025-02-22 23:22:31 -0500

    Removed location strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,7 +10,6 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    {provide: LocationStrategy, useClass: HashLocationStrategy},
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
